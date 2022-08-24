16789a98352fb478d02fa59465a6105e6c94c602
paola agudelo <paola10agudelo@gmail.com>
2022-08-24 15:04:47 -0500

    adding gitignore

--- src/test/java/Cucumber/Options/TestRunner.java
+++ src/test/java/Cucumber/Options/TestRunner.java
@@ -5,6 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class) /* to keep in mind io.cucumber.junit tiene version 4 no junit 5 por eso se usa este RunWith si no sería ExtendWith*/
-@CucumberOptions(features = "src/test/java/features",glue="stepDefinitions")
+@CucumberOptions(features = "src/test/java/features",
+		plugin="json:target/jsonReports/cucumber-report.json",
+		glue="stepDefinitions")
 public class TestRunner {
 }
